e52d0d445a820ca0beb445f95921e12a39ecb253
Hagen Mahnke <hagenmahnke@googlemail.com>
2014-08-06 16:36:36 +0200

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806142136) do
+ActiveRecord::Schema.define(version: 20140806143533) do
 
   create_table "photos", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "topics", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -21,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140806142136) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
