14f1c5219b89f1e87e8d9602e76c0c8626d31404
Hagen Mahnke <hagenmahnke@googlemail.com>
2014-08-06 16:23:05 +0200

    scaffold photo resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806141754) do
+ActiveRecord::Schema.define(version: 20140806142136) do
+
+  create_table "photos", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
