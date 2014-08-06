8381b676fe1ef72e2721a3ddc3a0f5f8b1a26451
Hagen Mahnke <hagenmahnke@googlemail.com>
2014-08-06 16:30:02 +0200

    avoid name conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, constraints: { subdomain: 'admin' }
 
   resources :prisoners, constraints: { id: /\d+/ }
-  get 'reddit_clone/:name', to: 'threads#show', constraints: { name: /t_\w+/ }
+  get 'reddit_clone/:name', to: 'topics#show', constraints: { name: /t_\w+/ }
   get 'reddit_clone/:name', to: 'users#show', constraints: { name: /u_\w+/ }
 
   resources :lotteries, constraints: Lottery.new
